420dcd5e9c6f549a280b7b6691fd24b06b2792ff
Hermawan Nugroho <hermawannugroho99@gmail.com>
2023-02-13 16:21:04 +0700

    init

---
+++ src/main/java/jquerypractice/App.java
@@ -0,0 +1,13 @@
+package jquerypractice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/StepDefinitions/Hooks.java
@@ -0,0 +1,43 @@
+package StepDefinitions;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.Duration;
+
+import org.apache.commons.io.FileUtils;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
+import Driver.Driver;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+
+public class Hooks {
+    WebDriver driver;
+
+    @Before
+    public void browserSetup() {
+        driver = Driver.getDriver();
+        driver.manage().window().maximize();
+    }
+
+    @After
+    public void tearDown(Scenario scenario) {
+        if (scenario.isFailed()) {
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+            String screenshotFile = scenario.getName() + ".png";
+            TakesScreenshot screenshotTaker = (TakesScreenshot) Driver.getDriver();
+            File screenshot = screenshotTaker.getScreenshotAs(OutputType.FILE);
+            try {
+                FileUtils.copyFile(screenshot, new File("target/screenshots/" + screenshotFile));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        driver.close();
+        driver.quit();
+    }
+}
